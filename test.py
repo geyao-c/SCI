ffe8a9b864ceb08f2686a29c51b243c5005a509b
geyao-c <1279846803@qq.com>
2023-03-06 20:56:06 +0800

    xiugai

--- test.py
+++ test.py
@@ -12,9 +12,9 @@ from model import Finetunemodel
 from multi_read_data import MemoryFriendlyLoader
 
 parser = argparse.ArgumentParser("SCI")
-parser.add_argument('--data_path', type=str, default='./data/LSRW_train',
+parser.add_argument('--data_path', type=str, default='./data/LSRW_eval',
                     help='location of the data corpus')
-parser.add_argument('--save_path', type=str, default='./results/LSRW_train', help='location of the data corpus')
+parser.add_argument('--save_path', type=str, default='./results/LSRW_eval', help='location of the data corpus')
 parser.add_argument('--model', type=str, default='./weights/difficult.pt', help='location of the data corpus')
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
 parser.add_argument('--seed', type=int, default=2, help='random seed')
